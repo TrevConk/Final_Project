36ea37a3d807f376ceb6501513f1baf0cf890f23
TrevConk <trevorconklin@gmail.com>
2021-12-08 15:17:16 -0500

    updated

--- open_page.py
+++ open_page.py
@@ -35,7 +35,7 @@ elif(currentPin != pin and pin != ''):
     print('''
     <html>
     <form action="/cgi-bin/open_page.py" method="POST">
-        </b>Please enter a Pin</b><br>
+        <b>Please enter a Pin</b><br>
         <input type="text" name="pin"><br>
         <input type="submit" name="submit" value="submit"><br>
     </input>
