efd60aa9189963579bd7256be5294e73694e18cc
TrevConk <trevorconklin@gmail.com>
2021-12-08 16:12:50 -0500

    updated cgitlog

--- open_page.py
+++ open_page.py
@@ -4,7 +4,7 @@ import cgitb
 import json
 
 #Setip for the creation of the new webpage
-cgitb.enable(display=1)
+cgitb.enable(display=0)
 data = cgi.FieldStorage()
 
 #setting up the different variables
